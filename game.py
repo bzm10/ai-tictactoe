d02c65d4f8641830df1b6a10baafa10b657f98e0
Benjamin M <137508630+bzm10@users.noreply.github.com>
2024-05-17 13:50:22 +0300

    Create game.py

---
+++ game.py
@@ -0,0 +1,8 @@
+# Import the TicTacToeAI class from the tictactoe module
+from ai_tictactoe import TicTacToeAI
+
+# Create an instance of the TicTacToeAI class
+game = TicTacToeAI()
+
+# Start playing the Tic Tac Toe game vs AI
+game.play()
